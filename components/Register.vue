03a4d28d5c0953da33b949a70f44e804d826e499
Andrea López <119761770+andrewswiftie@users.noreply.github.com>
2024-07-04 10:53:12 -0700

    Update Register.vue
    prueba

--- components/Register.vue
+++ components/Register.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <h1>Registro</h1>
             <form @submit.prevent="registerUser">
-                <label for="username">Nombre de usuario:</label>
+                <label for="username">Nombreeee de usuario:</label>
                 <input type="text" id="username" v-model="username" required><br><br>
                 <label for="email">Correo electrónico:</label>
                 <input type="email" id="email" v-model="email" required><br><br>
